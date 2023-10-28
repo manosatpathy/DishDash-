16d4aab4ac66f88b951799949eb2f13684422e17
manosatpathy <manosatpathy@gmail.com>
2023-10-28 23:58:25 +0530

    finishing restaurant menu component

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,15 +1,20 @@
-const MenuCard = () => {
+const MenuCard = (props) => {
   return (
     <div className="menuCard">
-   <div className="menuCardInfo">
-     <p>   &#9733;Bestseller </p>
-     <h4> Upvas Aloo Pattice with </h4>
-    <p>&#8377; 210</p>
+      <div className="menuCardInfo">
+        {props.data.card.info.ribbon.text ? (
+          <p> &#9733; {props.data.card.info.ribbon.text} </p>
+        ) : null}
+        <h4> {props.data.card.info.name} </h4>
+        <p>&#8377;{props.data.card.info.price / 100}</p>
+        <p>{props.data.card.info.description}</p>
+      </div>
+      <img className="dishImg" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" + props.data.card.info.imageId} />
+      <p>
+        ------------------------------------------------------------------------------------
+      </p>
     </div>
-    <img className="menuImg" src=""/>
-    <p>------------------------------------------------------------------------------------</p>
-    </div>
-  )
-}
+  );
+};
 
 export default MenuCard;
--- src/components/ResInfo.js
+++ src/components/ResInfo.js
@@ -2,52 +2,63 @@ import { useParams } from "react-router-dom";
 import MenuCard from "./MenuCard";
 import { resMenuApi } from "./config";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const ResInfo = () => {
-  const {resId} = useParams();
+  const { resId } = useParams();
 
-  const [resInfo, setResInfo] = useState([]);
+  const [resInfo, setResInfo] = useState(null);
 
-
- useEffect(()=>{
+  useEffect(() => {
     fetchResMenu();
- },[])
-
+  }, []);
 
   const fetchResMenu = async () => {
-    const response = await fetch(resMenuApi+resId);
+    const response = await fetch(resMenuApi + resId);
     const data = await response.json();
-    const allResMenu = data?.data?.cards[0]?.card?.card?.info;
-    setResInfo(allResMenu);
-    console.log(allResMenu)
+    setResInfo(data);
   };
 
-  return (
+  const { info } = resInfo?.data?.cards[0]?.card?.card || {};
+  const { itemCards } =
+    resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card || {};
+
+  console.log(itemCards);
+
+  return !info && !itemCards ? (
+    <Shimmer />
+  ) : (
     <div className="resInfoContainer">
       <div className="resInfoContainer">
         <div className="resInfo">
-          <h2>{resInfo.name}</h2>
-          <p>{resInfo.cuisines.join(", ")}</p>
-          <p>{resInfo.areaName}, 6.5 km</p>
-          <p>{resInfo.feeDetails.message}</p>
+          <h2>{info.name}</h2>
+          <p>{info.cuisines.join(", ")}</p>
+          <p>
+            {info.areaName}, {info.sla.lastMileTravel + " km"}
+          </p>
+          <p>{info.feeDetails.message}</p>
         </div>
         <div className="resRating">
-          <h3> &#9733; {resInfo.avgRating}</h3>
+          <h3> &#9733; {info.avgRating}</h3>
           <p>----------------</p>
-          <p>100+ ratings</p>
+          <p>{info.totalRatingsString}</p>
         </div>
       </div>
       <p>
         ------------------------------------------------------------------------
       </p>
-      <div className="cost">
-        <h2>35MINS</h2>
-        <h2>rs200 for two</h2>
+      <div className="time-cost">
+        <h2>{info.sla.deliveryTime + " MINS"}</h2>
+        <h2>{info.costForTwoMessage}</h2>
       </div>
       <p>
         ------------------------------------------------------------------------
       </p>
-      <MenuCard />
+
+      {itemCards.map((menu) => {
+        return <MenuCard data={menu} key={menu.card.info.id} />;
+      })}
     </div>
   );
 };
